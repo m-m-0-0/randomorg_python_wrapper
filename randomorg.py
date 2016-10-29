eb2f200fd7553d7230d9d084fccdf52f7376cd3a
Mauro Molari <molarimauro00@gmail.com>
2016-10-30 00:49:26 +0200

    updated exceptions and modified indentation

--- randomorg.py
+++ randomorg.py
@@ -6,8 +6,17 @@ import copy
 API_URL = "https://api.random.org/json-rpc/1/invoke"
 BASE_REQ = {"jsonrpc": "2.0", "id": 0, "params": {} }
 
-class api_exception(Exception):
+
+class KeyException(Exception):
     pass
+
+
+class ApiException(Exception):
+	pass
+
+
+class NetworkException(exception)
+	pass
 
 def buildRequest(basereq, *args, method=""):
     reqargs = copy.deepcopy(basereq)
@@ -24,25 +33,29 @@ def buildRequest(basereq, *args, method=""):
         reqargs["params"][key] = value
     return reqargs
 
+
 def apiRequest(reqargs):
     request = requests.post(API_URL, json = reqargs)
     response = json.loads(request.text)
     statuscode = request.status_code
+
     try:
         errorcode = response["error"]["code"]
         errormess = response["error"]["message"]
 
-        raise api_exception("%s: %s"%(errorcode,errormess))
+        raise KeyException("%s: %s"%(errorcode,errormess))
     
-    except api_exception:
+    except KeyException:
         raise
     except:
         pass
 
+
     if statuscode == 200:
         return response
     else:
-        raise api_exception(statuscode)
+        raise networkException(statuscode)
+
 
 class Randomorg:
     def __init__(self, apiKey):
@@ -53,60 +66,70 @@ class Randomorg:
         if req.status_code == 200:
             pass
         else:
-            raise api_exception(req.status_code)
+            raise NetworkException(req.status_code)
         
         response = json.loads(req.text)
         
         try:
             errorcode = response["error"]["code"]
             errormess = response["error"]["message"]
-            raise api_exception("%s: %s"%(errorcode,errormess))
+            raise ApiException("%s: %s"%(errorcode,errormess))
 
-        except api_exception:
+        except ApiException:
             raise
         except:
             pass
 
         if response["result"]["status"] == "stopped":
-            raise api_exception("Api key is stopped.")
+            raise ApiException("Api key is stopped.")
        
         if response["result"]["requestsLeft"] == 0:
-            raise api_exception("Requests limit reached.")
+            raise ApiException("Requests limit reached.")
 
         if response["result"]["bitsLeft"] == 0:
             print("WARNING: bits limit reached.")
+
+
 
     def status(self):
         response = apiRequest(buildRequest(self.basereq, method="getUsage"))
         return response["result"]["status"]
 
+
     def bitsLeft(self):
         response = apiRequest(buildRequest(self.basereq, method="getUsage"))
         return response["result"]["bitsLeft"]
+
 
     def requestsLeft(self):
         response = apiRequest(buildRequest(self.basereq, method="getUsage"))
         return response["result"]["requestsLeft"]
 
+
     def generateIntegers(self, n, min, max, replacement="true", base="10"):
         response = apiRequest(buildRequest(self.basereq, locals(), method = "generateIntegers"))
         return response["result"]["random"]["data"]
+
 
     def generateDecimalFractions(self, n, decimalPlaces, replacement="true"):
         response = apiRequest(buildRequest(self.basereq, locals(), method = "generateDecimalFractions"))
         return response["result"]["random"]["data"]
 
+
     def generateGaussians(self, n, mean, standardDeviation, significantDigits):
         response = apiRequest(buildRequest(self.basereq, locals(), method = "generateGaussians"))
         return response["result"]["random"]["data"]
+
 
     def generateStrings(self, n, length, characters, replacement="true"):
         response = apiRequest(buildRequest(self.basereq, locals(), method = "generateStrings"))
         return response["result"]["random"]["data"]
 
+
     def generateUUIDs(self, n):
         response = apiRequest(buildRequest(self.basereq, locals(), method = "generateUUIDs"))
         return response["result"]["random"]["data"]
+
 
     def generateBlobs(self, n, size, format="base64"):
         response = apiRequest(buildRequest(self.basereq, locals(), method = "generateBlobs"))
